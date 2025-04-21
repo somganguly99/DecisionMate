c1b79ad5065df807fd8525ad1f3a891f530e1f99
Som Ganguly <69516160+somganguly99@users.noreply.github.com>
2025-04-21 11:09:35 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
 
 const clientId = "178409291269-l5oueq8mf56gt3jr6u709a44g90qpnh7.apps.googleusercontent.com";
 
-if (!clientId) {
-  throw new Error("❌ Google Client ID is not defined. Set VITE_GOOGLE_CLIENT_ID in .env or Vercel.");
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={clientId}>
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
