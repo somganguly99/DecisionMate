bde8fec244a40092ad20e3677b2604d203a18291
Som Ganguly <69516160+somganguly99@users.noreply.github.com>
2025-04-24 17:37:29 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-const clientId = "178409291269-l5oueq8mf56gt3jr6u709a44g90qpnh7.apps.googleusercontent.com";
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+if (!clientId) {
+  throw new Error('Google Client ID is not defined in environment variables');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
